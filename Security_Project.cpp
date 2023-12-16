c7e499d5b7f4087e8298c980e7b8d7d3794f935b
Bishoy Yousry <1900733@eng.asu.edu.eg>
2023-12-16 04:51:35 +0200

    Add hash_asym_sym function, Error in ready public key file

--- Security_Project.cpp
+++ Security_Project.cpp
@@ -1,35 +1,28 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <memory>
-#include <stdexcept>
-#include <cstring>
-
 #include <openssl/aes.h>
 #include <openssl/err.h>
 #include <openssl/evp.h>
 #include <openssl/evperr.h>
 #include <openssl/crypto.h>
 #include <openssl/sha.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
+extern "C"
+{
+#include <openssl/applink.c>
+}
 
 #pragma warning(disable : 4996)
 
-
 #define BUFSIZE 1024
 
-
-
-int encrypt(unsigned char* plaintext, int plaintext_len, unsigned char* key,
-    unsigned char* iv, unsigned char* ciphertext);
-
-int decrypt(unsigned char* ciphertext, int ciphertext_len, unsigned char* key,
-    unsigned char* iv, unsigned char* plaintext);
-
+int encrypt(unsigned char* plaintext, int plaintext_len, unsigned char* key, unsigned char* iv, unsigned char* ciphertext);
+int decrypt(unsigned char* ciphertext, int ciphertext_len, unsigned char* key, unsigned char* iv, unsigned char* plaintext);
 void generateHash(const unsigned char* plain_text, size_t length, unsigned char*);
-
-void append(unsigned char* plainText, unsigned int plainSize,
-    unsigned char* encryptedHash);
-
+void append(unsigned char* plainText, unsigned int plainSize, unsigned char* encryptedHash);
+int encryptRSA(unsigned char* data, int dataLen, const char* keyFile, unsigned char* ciphertext);
+int decryptRSA(unsigned char* data, int dataLen, const char* KeyFile, unsigned char* decryptedtext);
+void hash_asym_sym(unsigned char* plainText, unsigned int plainTextLen, unsigned char* hashValue, const char* privateKeyFile, unsigned char* ciphertext);
 
 /* Function to encrypt data */
 int encrypt(unsigned char* plaintext, int plaintext_len, unsigned char* key,
@@ -134,6 +127,104 @@ void append(unsigned char* plainText, unsigned int plainSize,
 }
 
 
+/* Function to encrypt data using RSA */
+/* Function to encrypt data using RSA */
+int encryptRSA(unsigned char* plaintext, int plaintext_len, const char* publicKeyFile, unsigned char* ciphertext)
+{
+    size_t rsa_ciphertext_len;
+    FILE* publicKey;
+    fopen_s(&publicKey, publicKeyFile, "rb");
+    if (!publicKey) {
+        perror("Error opening public key file");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY* evp_key = PEM_read_PUBKEY(publicKey, NULL, NULL, NULL);
+    if (!evp_key) {
+        perror("Error reading public key");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new(evp_key, NULL);
+    if (!ctx) {
+        perror("Error creating context");
+        exit(EXIT_FAILURE);
+    }
+
+    if (EVP_PKEY_encrypt_init(ctx) <= 0 || EVP_PKEY_encrypt(ctx, ciphertext, &rsa_ciphertext_len, plaintext, plaintext_len) <= 0) {
+        perror("Error during encryption");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY_CTX_free(ctx);
+    EVP_PKEY_free(evp_key);
+    fclose(publicKey);
+
+    return rsa_ciphertext_len;
+}
+
+/* Function to decrypt data using RSA */
+int decryptRSA(unsigned char* ciphertext, int ciphertext_len, const char* privateKeyFile, unsigned char* decryptedtext)
+{
+    FILE* privateKey;
+    fopen_s(&privateKey, privateKeyFile, "rb");
+    if (!privateKey) {
+        perror("Error opening private key file");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY* evp_key = PEM_read_PrivateKey(privateKey, NULL, NULL, NULL);
+    if (!evp_key) {
+        perror("Error reading private key");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new(evp_key, NULL);
+    if (!ctx) {
+        perror("Error creating context");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t decryptedtext_len = 0;
+    if (EVP_PKEY_decrypt_init(ctx) <= 0 || EVP_PKEY_decrypt(ctx, NULL, &decryptedtext_len, ciphertext, ciphertext_len) <= 0) {
+        perror("Error during decryption (getting buffer size)");
+        exit(EXIT_FAILURE);
+    }
+
+    if (EVP_PKEY_decrypt(ctx, decryptedtext, &decryptedtext_len, ciphertext, ciphertext_len) <= 0) {
+        perror("Error during decryption");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY_CTX_free(ctx);
+    EVP_PKEY_free(evp_key);
+    fclose(privateKey);
+
+    // Null-terminate the decrypted text
+    decryptedtext[decryptedtext_len] = '\0';
+
+    return decryptedtext_len;
+}
+
+
+/* 
+   1- Generate hash value for the plain text using SHA512
+   2- Encrypt it in RSA using private key
+   3- Append it with the plain text 
+*/
+void hash_asym_sym(unsigned char* plainText, unsigned int plainTextLen,
+                   unsigned char* hashValue, const char* privateKeyFile,
+                   unsigned char* ciphertext)
+{
+    /* Generate hash value for the plain text using SHA512 */
+    generateHash(plainText, plainTextLen, hashValue);
+
+    /* Encrypt the hash using the private key */ 
+    encryptRSA(hashValue, SHA512_DIGEST_LENGTH * 2, privateKeyFile, ciphertext);
+
+    /* Append the plainText with the encrypted hash value (This is the data sent) */
+    append(plainText, plainTextLen, hashValue);
+}
 
 
 
@@ -161,15 +252,16 @@ int main()
     //decryptedtext[decryptedtext_len] = '\0';  // Null-terminate the decrypted text
     //printf("Decrypted text: %s\n", decryptedtext);
 
-    /* Sign */
-    // A sample plain text
-    unsigned char plainText[] = "Hello World!";
-    unsigned int plainSize = sizeof(plainText) - 1;
 
-    // Generate hash value for the plain text using SHA512
+    unsigned char plainText[] = "hello";
+    unsigned int plainTextLen = sizeof(plainText) - 1;
+    unsigned char ciphertext[BUFSIZE];
+
+    // Paths to your public and private key files
+    const char* publicKeyFile = "public.pem";
+    const char* privateKeyFile = "private.pem";
     unsigned char hashValue[SHA512_DIGEST_LENGTH];
-    generateHash(plainText, plainSize, hashValue);
 
-    /* Append the plainText with the encrypted hash value (This is the data sent) */
-    append(plainText, plainSize, hashValue);
+    hash_asym_sym(plainText, plainTextLen, hashValue, privateKeyFile, ciphertext);
+
 }
